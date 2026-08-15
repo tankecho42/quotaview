8c27ded2f5df5ccc06321615428eb3d30f803f78
derek.liu <derektanko@gmail.com>
2026-08-15 20:22:27 +0800

    v0.11.1: restore POST_PROMOTED_NOTIFICATIONS permission (Live Alerts toggle depends on it)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.tankecho.quotaview"
         minSdk = 26
         targetSdk = 36
-        versionCode = 24
-        versionName = "0.11.0"
+        versionCode = 25
+        versionName = "0.11.1"
     }
 
     buildTypes {
